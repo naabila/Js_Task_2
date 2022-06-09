ce3f40cb79cca74e6875eac7ebfd4fa55be19a4f
nabila Islam <nabilaislamhstu53@gmail.com>
2022-06-09 21:38:44 +0600

    modification

--- task2.js
+++ task2.js
@@ -1,3 +1,6 @@
 // Task 1
-var number = 23456;
-console.log("Decimal to binary number:"+number.toString(2)+ "  "+"and"+ " "+ "Decimal number:"+number.toString(10))
+var deciNumber = 23456;
+console.log("Decimal to binary number:"+deciNumber.toString(2));
+
+var binary_num= 101111001;
+console.log("Binary to decimal number:"+binary_num.toString(10));
