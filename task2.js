88ed3bd7b4bec244c245942d871f666af681d56d
nabila Islam <nabilaislamhstu53@gmail.com>
2022-06-09 21:32:39 +0600

    file modified

--- task2.js
+++ task2.js
@@ -1,13 +1,3 @@
-// var number= 700;
-// var to_binary= number.toString(2);
-// console.log(`The binary number is ${to_binary}`);
-
-
-// // Binary to decimal
-// var binary_num = 101001;
-// var to_decimal = binary_num.toString(10);
-// console.log(`The decimal number is ${to_decimal}`)
-
-
+// Task 1
 var number = 23456;
 console.log("Decimal to binary number:"+number.toString(2)+ "  "+"and"+ " "+ "Decimal number:"+number.toString(10))
